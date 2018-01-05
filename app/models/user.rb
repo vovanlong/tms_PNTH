175a64bc797d1a91a2df50220dc1a07d6b6c26e5
VanHau95 <vanhau1195@gmail.com>
2018-01-05 10:43:50 +0700

    log-in log-out show-edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,46 @@
 class User < ApplicationRecord
-  has_many :user_courses, dependent: :nullify
-  has_many :user_subjects, dependent: :destroy
-  has_many :user_tasks, dependent: :destroy
-  has_many :courses, through: :user_courses
-  has_many :subjects, through: :user_subjects
-  has_many :tasks, through: :user_tasks
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_accessor :remember_token
+  validates :email, presence: true, length: {maximum: Settings.email_max_lengh},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.password_min_lengh}, allow_nil: true
+  validates :name, presence: true, length: {maximum: Settings.name_max_lengh}
+  validates :address, length: {maximum: Settings.address_max_lengh}
+  validates :school, length: {maximum: Settings.school_max_lengh}
+  before_save :downcase_email
+  has_secure_password
+  scope :desc_created_at, ->{order(created_at: :desc)}
+  # log in - log out
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
