417614d606ef1be3466cecf14239b0ff6eeed366
UyTran <vanuy113@gmail.com>
2018-01-14 11:04:49 +0700

    user finish subject

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -3,14 +3,29 @@ class SubjectsController < ApplicationController
 
   def show
     @tasks = @subject.tasks
+    @user_subject = current_user.user_subjects.find_by subject_id: @subject.id
+    return if @user_subject
+    flash[:danger] = t "error"
+    redirect_to root_path
   end
 
   private
 
+  def load_course
+    @course = Course.find_by id: params[:course_id]
+    return if @course
+    flash[:danger] = t "wrong"
+    redirect_to root_path
+  end
+
+  def subject_params
+    params.require(:subject).permit :name, :description, :duration, :picture
+  end
+
   def load_subject
     @subject = Subject.find_by id: params[:id]
     return if @subject
-    flash[:danger] = I18n.t "courses.load_subject.not_found"
+    flash[:danger] = t "courses.load_subject.not_found"
     redirect_to root_path
   end
 end
--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -1,4 +1,6 @@
 class UserSubject < ApplicationRecord
   belongs_to :user
   belongs_to :subject
+  enum status: {init: 0, finished: 1}
+  delegate :name, :description, :duration, to: :subject, prefix: true
 end
