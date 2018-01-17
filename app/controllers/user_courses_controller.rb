21445d9bd7a1614cdb829a77a19102abf0466fe2
UyTran <vanuy113@gmail.com>
2018-01-17 17:25:57 +0700

    just one course is active each user, show modal in course

--- app/controllers/user_courses_controller.rb
+++ app/controllers/user_courses_controller.rb
@@ -3,18 +3,27 @@ class UserCoursesController < ApplicationController
   before_action :logged_in_user
 
   def update
-    if @user_course.update user_course_params
-      respond_to do |format|
-        format.html{redirect_back fallback_location: @user_course.course}
-        format.js
+    if @user_course.joined?
+      if current_user.course_active_size.zero?
+        update_status
+      else
+        flash[:danger] = t "controllers.user_courses.update.check_active"
+        redirect_back fallback_location: @user_course.course
       end
     else
-      flash[:danger] = t "controllers.user_courses.update.failed"
+      update_status
       redirect_back fallback_location: @user_course.course
     end
   end
 
   private
+
+  def update_status
+    respond_to do |format|
+      format.html{redirect_back fallback_location: @user_course.course}
+      format.js
+    end
+  end
 
   def load_user_course
     @user_course = UserCourse.find_by id: params[:id]
